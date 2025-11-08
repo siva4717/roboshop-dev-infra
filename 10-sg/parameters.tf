07221fadd9f6c646a474962144e9abc5a7f24a2d
siva4717 <sivagopi@4717>
2025-11-08 14:48:44 +0530

    roboshop-dev-infra

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "sg_id" {
   count = length(var.sg_names)
   name  = "/${var.project_name}/${var.environment}/${var.sg_names[count.index]}_sg_id"
   type  = "String"
-  value = module.this[count.index].sg_id
+  value = module.sg[count.index].sg_id
 }
 
 
