1dbf0f16651c2e67b288fe1a0d00e928a1c0b4c7
siva4717 <sivagopi@4717>
2025-11-08 15:07:46 +0530

    roboshop-dev-infra

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -1,91 +1,84 @@
+
+
 resource "aws_security_group_rule" "backend_alb_bastion" {
   type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = local.security_group_id
+  security_group_id = local.backend_alb_sg_id
   source_security_group_id = local.bastion_sg_id
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
 }
 
-resource "aws_security_group_rule" "bastion" {
+
+resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
   security_group_id = local.bastion_sg_id
   cidr_blocks = ["0.0.0.0/0"]
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
 }
 
-resource "aws_security_group_role" "mongodb_bastion"{
-  type = "ingress"
-  from_port = 22 
-  to_port = 22 
-  protocal = "tcp"
+resource "aws_security_group_rule" "mongodb_bastion" {
+  type              = "ingress"
   security_group_id = local.mongodb_sg_id
   source_security_group_id = local.bastion_sg_id
-
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
 }
 
-resource "aws_security_group_role" "redis_bastion"{
-  type = "ingress"
-  from_port = 22 
-  to_port = 22 
-  protocal = "tcp"
+resource "aws_security_group_rule" "redis_bastion" {
+  type              = "ingress"
   security_group_id = local.redis_sg_id
   source_security_group_id = local.bastion_sg_id
-
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
 }
 
-
-resource "aws_security_group_role" "mysql_bastion"{
-  type = "ingress"
-  from_port = 22 
-  to_port = 22 
-  protocal = "tcp"
-  security_group_id = local.mysql_sg_id
-  source_security_group_id = local.bastion_sg_id
-
-}
-
-resource "aws_security_group_role" "rabbitmq_bastion"{
-  type = "ingress"
-  from_port = 22 
-  to_port = 22 
-  protocal = "tcp"
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type              = "ingress"
   security_group_id = local.rabbitmq_sg_id
   source_security_group_id = local.bastion_sg_id
-
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
 }
 
-resource "aws_security_group_role" "catalogue_bastion"{
-  type = "ingress"
-  from_port = 22 
-  to_port = 22 
-  protocal = "tcp"
+# Created as part of ticket 1234GDF
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  security_group_id = local.mysql_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
+
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
   security_group_id = local.catalogue_sg_id
   source_security_group_id = local.bastion_sg_id
-
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
 }
 
-resource "aws_security_group_role" "mongodb_catalogue"{
-  type = "ingress"
-  from_port = 27017 
-  to_port = 27017
-  protocal = "tcp"
-  security_group_id = local.catalogue_sg_id
-  source_security_group_id = local.mongodb_sg_id
-
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  security_group_id = local.mongodb_sg_id
+  source_security_group_id = local.catalogue_sg_id
+  from_port         = 27017
+  protocol          = "tcp"
+  to_port           = 27017
 }
 
-resource "aws_security_group_role" "catalogue_backend_alb"{
-  type = "ingress"
-  from_port = 8080
-  to_port = 8080
-  protocal = "tcp"
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
   security_group_id = local.catalogue_sg_id
   source_security_group_id = local.backend_alb_sg_id
-
+  from_port         = 8080
+  protocol          = "tcp"
+  to_port           = 8080
 }
-
-
-
