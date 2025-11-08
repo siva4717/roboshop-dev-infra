91a48c38b5b291415f306290e23352e3a761b01e
siva4717 <sivagopi@4717>
2025-11-08 14:59:19 +0530

    roboshop-dev-infra

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -1,23 +1,21 @@
 data "aws_ami" "joindevops" {
- 
-  owners           = ["973714476881"]
-  most_recent      = true
- 
+    owners           = ["973714476881"]
+    most_recent      = true
+    
+    filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
 
-  filter {
-    name   = "DevOpsPractice"
-    values = ["RHEL-9-DevOps-Practice"]
-  }
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
 }
 
 
