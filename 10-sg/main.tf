0a9470cde44eb79a7d8d1bdfc15b29385d5b8d5a
siva4717 <sivagopi@4717>
2025-11-08 14:45:57 +0530

    roboshop-dev-infra

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,8 +1,34 @@
-module "this" {
+
+
+
+# Using Open source module
+/* module "catalogue" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "${local.common_name_suffix}-catalogue"
+  use_name_prefix = false
+  description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
+  vpc_id      = data.aws_ssm_parameter.vpc_id.value
+
+} */
+
+
+module "sg" {
   count = length(var.sg_names)
   source = "git::https://github.com/siva4717/terraform-aws-sg.git?ref=main"
-  sg_name=var.sg_names[count.index]
-  project_name=var.project_name
-  environment=var.environment
-  vpc_id=local.vpc_id
+  project_name = var.project_name
+  environment = var.environment
+  sg_name = var.sg_names[count.index]
+  
+  vpc_id =  local.vpc_id
 }
+
+# Frontend accepting traffic from frontend ALB
+# resource "aws_security_group_rule" "frontend_frontend_alb" {
+#   type              = "ingress"
+#   security_group_id = module.sg[9].sg_id # frontend SG ID
+#   source_security_group_id = module.sg[11].sg_id # frontend ALB SG ID
+#   from_port         = 80
+#   protocol          = "tcp"
+#   to_port           = 80
+# }
