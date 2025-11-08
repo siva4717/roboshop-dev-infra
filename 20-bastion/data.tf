1305e130a9fc4f8994b9e942cb94f3092f283f00
siva4717 <sivagopi@4717>
2025-11-08 14:53:09 +0530

    roboshop-dev-infra

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -19,3 +19,12 @@ data "aws_ami" "joindevops" {
     values = ["hvm"]
   }
 }
+
+
+data "aws_ssm_parameter" "bastion_sg_id" {
+  name = "/${var.project_name}/${var.environment}/bastion_sg_id"
+}
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+}
