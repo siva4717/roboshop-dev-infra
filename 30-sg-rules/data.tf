b338e8ddce10aa8109b0f6736a3326b4f9d4b7a7
siva4717 <sivagopi@4717>
2025-11-08 15:10:44 +0530

    roboshop-dev-infra

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -1,30 +1,30 @@
 data "aws_ssm_parameter" "backend_alb_sg_id" {
-  name="/${var.project_name}/${var.environment}/backend_alb"
+  name="/${var.project_name}/${var.environment}/backend_alb_sg_id"
 }
 
 
 data "aws_ssm_parameter" "bastion_sg_id" {
-  name="/${var.project_name}/${var.environment}/bastion"
+  name="/${var.project_name}/${var.environment}/bastion_sg_id"
 }
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
-  name="/${var.project_name}/${var.environment}/mongodb"
+  name="/${var.project_name}/${var.environment}/mongodbg_sg_id"
 }
 
 data "aws_ssm_parameter" "redis_sg_id" {
-  name="/${var.project_name}/${var.environment}/redis"
+  name="/${var.project_name}/${var.environment}/redis_sg_id"
 }
 
 data "aws_ssm_parameter" "mysql_sg_id" {
-  name="/${var.project_name}/${var.environment}/mysql"
+  name="/${var.project_name}/${var.environment}/mysql_sg_id"
 }
 
 data "aws_ssm_parameter" "rabbitmq_sg_id" {
-  name="/${var.project_name}/${var.environment}/rabbitmq"
+  name="/${var.project_name}/${var.environment}/rabbitmq_sg_id"
 }
 
 data "aws_ssm_parameter" "catalogue_sg_id" {
-  name="/${var.project_name}/${var.environment}/catalogue"
+  name="/${var.project_name}/${var.environment}/catalogue_sg_id"
 }
 
 
