6f169edbaff9b1733d228a02cf4aabd317272075
siva4717 <sivagopi@4717>
2025-11-09 17:13:13 +0530

    roboshop-dev-infra

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -1,44 +1,42 @@
+
+
 data "aws_ami" "joindevops" {
- 
-  owners           = ["973714476881"]
-  most_recent      = true
- 
+    owners           = ["973714476881"]
+    most_recent      = true
+    
+    filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
 
-  filter {
-    name   = "DevOpsPractice"
-    values = ["RHEL-9-DevOps-Practice"]
-  }
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
 }
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
-  name="/${var.project_name}/${var.environment}/mongodb"
+  name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
 }
 
 data "aws_ssm_parameter" "redis_sg_id" {
-  name="/${var.project_name}/${var.environment}/redis"
-}
-
-data "aws_ssm_parameter" "mysql_sg_id" {
-  name="/${var.project_name}/${var.environment}/mysql"
+  name = "/${var.project_name}/${var.environment}/redis_sg_id"
 }
 
 data "aws_ssm_parameter" "rabbitmq_sg_id" {
-  name="/${var.project_name}/${var.environment}/rabbitmq"
+  name = "/${var.project_name}/${var.environment}/rabbitmq_sg_id"
+}
+
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project_name}/${var.environment}/mysql_sg_id"
 }
 
 
-
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name  = "/${var.project_name}/${var.environment}/private_subnet_ids"
-  
+data "aws_ssm_parameter" "database_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/database_subnet_ids"
 }
